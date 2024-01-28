eb5be8698d20a3ca03403a452c1f781e98045fe2
HunterTigerX <HunterTigerX@gmail.com>
2024-01-28 23:48:22 +0300

    feat: complete fs delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+// implement function that deletes file fileToRemove.txt (if there's no file
+// fileToRemove.txt Error with message FS operation failed must be thrown)
+
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const remove = async () => {
-    // Write your code here 
+  if (!fs.existsSync(join(__dirname, "files", "fileToRemove.txt"))) {
+    throw new Error("FS operation failed: File fileToRemove.txt don't exist");
+  } else {
+    fs.unlink(join(__dirname, "files", "fileToRemove.txt"), (err) => {
+      if (err) {
+        console.log(err.message);
+        throw err;
+      }
+    });
+  }
 };
 
 await remove();
