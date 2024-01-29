779795672661c6becb8e033e5e3bd5e836d68354
HunterTigerX <HunterTigerX@gmail.com>
2024-01-29 05:13:19 +0300

    feat: complete zip decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+//  implement function that decompresses archive.gz back to the fileToCompress.txt
+// with same content as before compression using zlib and Streams API
+
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { createReadStream, createWriteStream } from "fs";
+import { createUnzip } from "zlib";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const decompressedFile = join(__dirname, "files", "fileToCompress.txt");
+const compressedFile = join(__dirname, "files", "archive.gz");
+
 const decompress = async () => {
-    // Write your code here 
+  const handleStream = createReadStream(compressedFile);
+  handleStream
+    .pipe(createUnzip())
+    .pipe(createWriteStream(decompressedFile))
+    .on("finish", () => {
+      console.log(`Decompression process done`);
+    });
 };
 
 await decompress();
