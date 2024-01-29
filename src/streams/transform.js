c10c6e6ed67bc47a3f3d7a41291063ef4f3485db
HunterTigerX <HunterTigerX@gmail.com>
2024-01-29 04:03:28 +0300

    feat: complete streams transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+// implement function that writes process.stdin data into file fileToWrite.txt
+// content using Writable Stream
+
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fileToWrite = join(__dirname, "files", "fileToWrite.txt");
+
 const transform = async () => {
-    // Write your code here 
+  const writable = fs.createWriteStream(fileToWrite);
+  process.stdin.pipe(writable);
 };
 
 await transform();
