35da99b71362cc3b48275bac00670f71d917c8ce
HunterTigerX <HunterTigerX@gmail.com>
2024-01-29 04:38:33 +0300

    feat: complete streams write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,31 @@
+// implement function that reads data from process.stdin, reverses text
+// using Transform Stream and then writes it into process.stdout
+
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import stream from "stream";
+const Transform = stream.Transform || import("readable-stream").Transform;
+import fs from "fs";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fileToWrite = join(__dirname, "files", "fileToWrite.txt");
+
 const write = async () => {
-    // Write your code here 
+  const writable = fs.createWriteStream(fileToWrite);
+  const reverseStream = new Transform({
+    transform(data, encoding, callback) {
+      const reversedData =
+        data
+          .toString()
+          .replace(/\n/g, "")
+          .split("")
+          .reverse()
+          .join("")
+          .trimStart() + "\n";
+      this.push(reversedData);
+      callback();
+    },
+  });
+  process.stdin.pipe(reverseStream).pipe(writable);
 };
 
 await write();
