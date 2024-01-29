b9c531a7732fb65436d0213d27085f26441d8fbd
HunterTigerX <HunterTigerX@gmail.com>
2024-01-29 05:04:41 +0300

    feat: complete zip compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+// implement function that compresses file fileToCompress.txt to archive.gz using
+// zlib and Streams API
+
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fileToCompress = join(__dirname, "files", "fileToCompress.txt");
+const compressedFile = join(__dirname, "files", "archive.gz");
+
 const compress = async () => {
-    // Write your code here 
+  const handleStream = createReadStream(fileToCompress);
+  handleStream
+    .pipe(createGzip())
+    .pipe(createWriteStream(compressedFile))
+    .on("finish", () => {
+      console.log(`Compression process done`);
+    });
 };
 
 await compress();
